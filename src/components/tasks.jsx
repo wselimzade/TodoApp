175c2a5cc2de6c1bd449cda90aafb8ae78456ee1
wselimzade <wselimzade@mail.ru>
2023-09-17 10:09:07 +0400

    elimination of design errors

--- src/components/tasks.jsx
+++ src/components/tasks.jsx
@@ -5,16 +5,20 @@ export default function Tasks() {
     return (
         <div className="tasks">
             <div className='task'>
-                <GoDot className='dot' /> Go Shopping
+                <GoDot className='dot' />
+                <span className='task-text'> Go Shopping </span>
             </div>
             <div className='task'>
-                <GoDot className='dot' /> Go Shopping
+                <GoDot className='dot' />
+                <span className='task-text'> Go Shopping </span>
             </div>
             <div className='task'>
-                <GoDot className='dot' /> Go Shopping
+                <GoDot className='dot' />
+                <span className='task-text'> Go Shopping </span>
             </div>
             <div className='task'>
-                <GoDot className='dot' /> Go Shopping
+                <GoDot className='dot' />
+                <span className='task-text'> Go Shopping </span>
             </div>
         </div>
     )
