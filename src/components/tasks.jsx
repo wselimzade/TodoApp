f37a1fe03641912372e409938403b5a5ad5234ee
wselimzade <wselimzade@mail.ru>
2023-09-17 04:52:01 +0400

    Correction of the main design

--- src/components/tasks.jsx
+++ src/components/tasks.jsx
@@ -1,7 +1,21 @@
+import { GoDot } from 'react-icons/go'
+import "../style/tasks.css"
+
 export default function Tasks() {
     return (
         <div className="tasks">
-            tasks
+            <div className='task'>
+                <GoDot className='dot' /> Go Shopping
+            </div>
+            <div className='task'>
+                <GoDot className='dot' /> Go Shopping
+            </div>
+            <div className='task'>
+                <GoDot className='dot' /> Go Shopping
+            </div>
+            <div className='task'>
+                <GoDot className='dot' /> Go Shopping
+            </div>
         </div>
     )
 }
