070d453fa8a2f9a635a35056ccafb02c782e03ba
wselimzade <wselimzade@mail.ru>
2023-09-17 10:00:17 +0400

    Completed component design

--- src/components/completed.jsx
+++ src/components/completed.jsx
@@ -1,7 +1,28 @@
+import "../style/completed.css"
+import { GoDotFill } from "react-icons/go"
+
 export default function Completed() {
     return (
         <div className="completed">
-            completed
+            <div className="completed-title"> Completed </div>
+            <div className="completed-tasks">
+                <div className='completed-task'>
+                    <GoDotFill className='dot' />
+                    <span className="completed-task-text"> Go Shopping </span>
+                </div>
+                <div className='completed-task'>
+                    <GoDotFill className='dot' />
+                    <span className="completed-task-text"> Go Shopping </span>
+                </div>
+                <div className='completed-task'>
+                    <GoDotFill className='dot' />
+                    <span className="completed-task-text"> Go Shopping </span>
+                </div>
+                <div className='completed-task'>
+                    <GoDotFill className='dot' />
+                    <span className="completed-task-text"> Go Shopping </span>
+                </div>
+            </div>
         </div>
     )
 }
